d2eae7a2d540cbb5ebdf18f8054a1833e34af89e
ehernandez <ehernandez@xumak.com>
2017-07-19 14:32:28 -0600

    Added Dockerfile for go-client

--- goClient/main.go
+++ goClient/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	pb "github.com/ehernandez-xk/grpc-demo/service"
@@ -9,10 +10,18 @@ import (
 )
 
 const (
-	address = "localhost:50051"
+	port = "50051"
 )
 
 func main() {
+
+	//flags
+	target := flag.String("target", "localhost", "target to connect")
+	name := flag.String("name", "no-name", "Your name")
+	flag.Parse()
+
+	log.Println(*target, *name)
+	address := *target + ":" + port
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect %v", err)
@@ -22,9 +31,9 @@ func main() {
 	c := pb.NewMyServiceClient(conn)
 
 	// Add a person
-	r, err := c.AddPerson(context.Background(), &pb.Person{Name: "Pepe"})
+	r, err := c.AddPerson(context.Background(), &pb.Person{Name: *name})
 	if err != nil {
 		log.Fatalf("Could not add name %v", err)
 	}
-	log.Printf("AddPerson: %v", r.Status)
+	log.Printf("AddPerson replay: %v", r.Status)
 }
